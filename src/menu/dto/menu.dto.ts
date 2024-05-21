0e079b361e7fe55ae88de13400dd3a17772914fb
Marcelo Miguel Ferreira <marmiguelferreira9@gmail.com>
2024-05-21 18:27:56 -0300

    Push pequenos ajustes nos dtos

--- src/menu/dto/menu.dto.ts
+++ src/menu/dto/menu.dto.ts
@@ -2,9 +2,6 @@ import { PartialType } from "@nestjs/mapped-types";
 import { IsInt, IsString, Length } from "class-validator";
 
 export class CreateMenuDto {
-    
-    @IsInt()
-    resturant_id: number;
 
     @IsString()
     @Length(1,40, {message: ' o tamanho minimo do nome é 1 caracter'})
