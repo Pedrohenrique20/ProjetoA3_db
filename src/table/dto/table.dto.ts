f205dde70ebec5e53e3dcf30e0dfe57a73064ee6
Pedro Henrique Chimbata Barreto <pedrochimbata@gmail.com>
2024-05-21 21:44:41 -0300

    Update table.dto.ts

--- src/table/dto/table.dto.ts
+++ src/table/dto/table.dto.ts
@@ -10,7 +10,7 @@ export class CreateTableDto {
     table_capacity: number;
 
     @IsString()
-    @Length(1,255, {message: 'o tamanho máximo do nome é 8 caracteres'})
+    @Length(1,255, {message: 'o tamanho máximo é de 255 caracteres'})
     table_details: string
 
     @IsInt()
